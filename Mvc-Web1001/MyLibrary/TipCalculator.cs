e521d6d284907664f485ee56f8e92d7d701b2063
Mariam Zahid <mzahidmalhi@hotmail.com>
2023-10-13 00:34:48 -0400

    Third method added

--- Mvc-Web1001/MyLibrary/TipCalculator.cs
+++ Mvc-Web1001/MyLibrary/TipCalculator.cs
@@ -4,6 +4,32 @@ public class TipCalculator
 {
     public decimal CalculateTipPerPerson(decimal totalPrice, int numberOfPatrons, float tipPercentage)
     {
+        public Dictionary<string, decimal> CalculateTipForEachPerson(Dictionary<string, decimal> mealCosts, float tipPercentage)
+    {
+        if (mealCosts == null || mealCosts.Count == 0 || tipPercentage < 0)
+        {
+            throw new ArgumentException("Invalid input values.");
+        }
+
+        decimal totalCost = 0;
+        foreach (var cost in mealCosts.Values)
+        {
+            totalCost += cost;
+        }
+
+        decimal totalTip = totalCost * (decimal)(tipPercentage / 100);
+        decimal tipPerPerson = totalTip / mealCosts.Count;
+
+        Dictionary<string, decimal> tipAmounts = new Dictionary<string, decimal>();
+        foreach (var pair in mealCosts)
+        {
+            decimal tip = pair.Value * (decimal)(tipPercentage / 100);
+            tipAmounts[pair.Key] = tipPerPerson + tip;
+        }
+
+        return tipAmounts;
+    }
+}
         if (totalPrice <= 0 || numberOfPatrons <= 0 || tipPercentage < 0)
         {
             throw new ArgumentException("Invalid input values.");
@@ -14,4 +40,3 @@ public class TipCalculator
 
         return tipPerPerson;
     }
-}
